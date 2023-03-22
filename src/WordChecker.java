eca8c37bec6825dea57746611dbf54585d89f8b6
Andrey <kovorot0875@mail.ru>
2023-03-22 23:08:59 +0300

    HashWord added

--- src/WordChecker.java
+++ src/WordChecker.java
@@ -5,4 +5,15 @@ public class WordChecker {
     public WordChecker (String text) {
         this.text = text;
     }
+
+    public boolean hashCode(String input) {
+        String text = this.text.toLowerCase();
+        String word = input.toLowerCase();
+
+        if (text.contains(word)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
