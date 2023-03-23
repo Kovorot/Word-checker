3b29fd50b1e4d4e4c5f63f86cb5b9e77f23e31b4
Andrey <kovorot0875@mail.ru>
2023-03-23 10:31:52 +0300

    HashWord modifed

--- src/WordChecker.java
+++ src/WordChecker.java
@@ -1,4 +1,9 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class WordChecker {
+    private static Set<String> words = new HashSet<>();
 
     protected String text;
 
@@ -6,14 +11,24 @@ public class WordChecker {
         this.text = text;
     }
 
-    public boolean hashCode(String input) {
+    public boolean hashWord(String input) {
         String text = this.text.toLowerCase();
-        String word = input.toLowerCase();
+        words.addAll(List.of(text.split("\\P{IsAlphabetic}+")));
 
-        if (text.contains(word)) {
+        if (words.contains(input.toLowerCase())) {
+            System.out.println("Слово есть");
             return true;
         } else {
+            System.out.println("Слова нет");
             return false;
         }
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
     }
 }
