363ff7c356024daba80d548f48a2568baa092b7c
Andrey <kovorot0875@mail.ru>
2023-03-23 10:37:18 +0300

    Demonstration added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        WordChecker checker = new WordChecker("Основной функцией системы страхования является защита имущественных " +
+                "интересов страхователей.");
 
+        checker.hashWord("Анализ");    //false
+        checker.hashWord("Защита");    //true
+        checker.hashWord("труды");     //false
+        checker.hashWord("система");   //false
+        checker.hashWord("интересов"); //true
     }
 }
